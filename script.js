d5d4dd4f83c5cd94d455833dee71935a83910ef9
minidu1 <lmmdsilva@students.nsbm.ac.lk>
2025-11-27 16:05:27 +0530

    made the draw function
    -create a basic func to track is mouse clicked and hold, then use it to draw

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const sketch = document.getElementById("sketch");
 
-
+// added a var to track if mouse is hold?
+let mouseDown = false; // default is mouse is "not clicked"
+document.body.onmousedown = ()=> mouseDown = true; 
+document.body.onmouseup = ()=> mouseDown= false;//mpusedown func will true untill mouse up
+console.log( mouseDown)
 for (let i = 0; i < 16; i++) {
     const row = document.createElement("div")
     row.className = "row"
-    sketch.appendChild(row) 
+    sketch.appendChild(row) //add the row to sketch. untill it wouldnt work. 
+
     for (let j = 0; j < 16; j++) {
         //need to put this inside a flex-box
         const box = document.createElement("div");
         box.className = "box"
-        box.textContent = j;
+        row.appendChild(box);//add boxes to the row
 
-        row.appendChild(box);
+        box.addEventListener("mouseover", ()=>{
+        if (mouseDown) {
+            box.classList.add("hover");
+        }
+    });// after innerloop end. a new row will be added
     }
 }
+
