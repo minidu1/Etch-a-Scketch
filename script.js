fd177f7a2b08a8a83c5d66686ec5d110d1a31144
minidu1 <lmmdsilva@students.nsbm.ac.lk>
2025-12-03 21:12:08 +0530

    add change grid function basic

--- script.js
+++ script.js
@@ -1,44 +1,66 @@
-function startPaint(box){
+function makeTheGrid(gridSize = 16) {
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement("div")
+        row.className = "row"
+        sketch.appendChild(row) //add the row to sketch. untill it wouldnt work. 
+
+        for (let j = 0; j < gridSize; j++) {
+            //need to put this inside a flex-box
+            const box = document.createElement("div");
+            box.className = "box"
+            row.appendChild(box);//add boxes to the row
+
+            box.addEventListener("mousedown", () => startPaint(box))
+            box.addEventListener("mouseover", () => paintBox(box));
+            // after innerloop end. a new row will be added
+        }
+    }
+}
+
+function startPaint(box) {
     box.classList.add("hover");
     mouseDown = true;
 }
 
-function paintBox(box){
+function paintBox(box) {
     if (mouseDown) {
-            box.classList.add("hover");
-        }
-}
-
-function clearGrid(){
-    allBoxes.forEach(box => box.classList.remove("hover"));
-}
-
-const sketch = document.getElementById("sketch");
-
-// added a var to track if mouse is hold?
-let mouseDown = false; // default is mouse is "not clicked"
-document.body.onmousedown = ()=> mouseDown = true; 
-document.body.onmouseup = ()=> mouseDown= false;//mpusedown func will true untill mouse up
-
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div")
-    row.className = "row"
-    sketch.appendChild(row) //add the row to sketch. untill it wouldnt work. 
-
-    for (let j = 0; j < 16; j++) {
-        //need to put this inside a flex-box
-        const box = document.createElement("div");
-        box.className = "box"
-        row.appendChild(box);//add boxes to the row
-
-        box.addEventListener("mousedown", ()=> startPaint(box))
-        box.addEventListener("mouseover", ()=> paintBox(box));
-        // after innerloop end. a new row will be added
+        box.classList.add("hover");
     }
 }
 
-const reset = document.querySelector(".reset");
-const allBoxes = document.querySelectorAll(".box");
+function clearPaint() {
+    const allBoxes = document.querySelectorAll(".box");
+    allBoxes.forEach(box => box.classList.remove("hover"));
+}
 
-reset.addEventListener("click", ()=> clearGrid())
-    
+function clearGrid(){
+    const allBoxes = document.querySelectorAll(".box");
+    const allRows = document.querySelectorAll(".row");
+    allBoxes.forEach(box => box.remove());
+    allRows.forEach(row => row.remove());
+
+}   
+
+const sketch = document.getElementById("sketch");
+
+makeTheGrid();
+
+// added a var to track if mouse is hold?
+let mouseDown = false; // default is mouse is "not clicked"
+document.body.onmousedown = () => mouseDown = true;
+document.body.onmouseup = () => mouseDown = false;//mpusedown func will true untill mouse up
+
+const reset = document.querySelector(".reset");
+
+reset.addEventListener("click", () => clearPaint())
+
+const changeSizeButton = document.querySelector(".change-size");
+
+changeSizeButton.addEventListener("click", () => {
+    gridSize = window.prompt("Enter the grid size. Please enter a number low than 100")
+    if (gridSize <= 100){
+        clearGrid();
+
+        makeTheGrid(gridSize);
+    }
+})
